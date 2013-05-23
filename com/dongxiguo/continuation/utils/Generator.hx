1719fbe65b658cecd7f43203511b3b99fa1401a4
杨博 <pop.atry@gmail.com>
2013-05-23 20:26:26 +0800

    Add Generator.hx

---
+++ com/dongxiguo/continuation/utils/Generator.hx
@@ -0,0 +1,165 @@
+// Copyright (c) 2013, 杨博 (Yang Bo)
+// All rights reserved.
+// 
+// Author: 杨博 (Yang Bo) <pop.atry@gmail.com>
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the <ORGANIZATION> nor the names of its contributors
+//   may be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package com.dongxiguo.continuation.utils;
+
+/**
+  An Iterator that contains yield result.
+**/
+@:final
+class Generator<Element>
+{
+  var status:IteratorStatus<Element>;
+  
+  public function new(runFunction:RunFunction<Element>)
+  {
+    #if (haxe_211 || haxe3)
+    this.status = UNKNOWN(runFunction.bind(this.yield, this.end));
+    #else
+    this.status = UNKNOWN(callback(runFunction, this.yield, this.end));
+    #end
+  }
+  
+  function end():Void
+  {
+    switch (this.status)
+    {
+      case UNKNOWN(_):
+        this.status = NO_NEXT;
+      default:
+        throw "Must not fork threads in a generator!";
+    }
+  }
+  
+  function yield(nextValue:Element, handler:Void->Void):Void
+  {
+    switch (this.status)
+    {
+      case UNKNOWN(_):
+        this.status = HAS_NEXT(nextValue, handler);
+      default:
+        throw "Must not fork threads in a generator!";
+    }
+  }
+  
+  public function next():Null<Element>
+  {
+    switch (this.status)
+    {
+      case UNKNOWN(fetchFunction):
+      {
+        fetchFunction();
+        switch (this.status)
+        {
+          case NO_NEXT:
+          {
+            return null;
+          }
+          case HAS_NEXT(nextValue, fetchFunction):
+          {
+            this.status = UNKNOWN(fetchFunction);
+            return nextValue;
+          }
+          case UNKNOWN(_):
+          {
+            return throw "Expect yield or return.";
+          }
+        }
+      }
+      case NO_NEXT:
+      {
+        return null;
+      }
+      case HAS_NEXT(nextValue, fetchFunction):
+      {
+        this.status = UNKNOWN(fetchFunction);
+        return nextValue;
+      }
+    }
+
+  }
+  
+  public function hasNext():Bool
+  {
+    switch (this.status)
+    {
+      case UNKNOWN(fetchFunction):
+      {
+        fetchFunction();
+        switch (this.status)
+        {
+          case NO_NEXT:
+          {
+            return false;
+          }
+          case HAS_NEXT(_, _):
+          {
+            return true;
+          }
+          case UNKNOWN(_):
+          {
+            return throw "Expect yield or return.";
+          }
+        }
+      }
+      case NO_NEXT:
+      {
+        return false;
+      }
+      case HAS_NEXT(_, _):
+      {
+        return true;
+      }
+    }
+  }
+  
+  public static function iterator<Element>(self:RunFunction<Element>):Generator<Element>
+  {
+    return new Generator(self);
+  }
+  
+  public static function toIterable<Element>(self:RunFunction<Element>):Iterable<Element>
+  {
+    return
+    {
+      iterator: function() { return new Generator(self); },
+    }
+  }
+}
+
+typedef RunFunction<Element> = YieldFunction<Element>->(Void->Void)->Void;
+
+typedef YieldFunction<Element> = Element->(Void->Void)->Void;
+
+private enum IteratorStatus<Element>
+{
+  UNKNOWN(fetchFunction:Void->Void);
+  HAS_NEXT(nextValue:Element, fetchFunction:Void->Void);
+  NO_NEXT;
+}
---
+++ tests/TestGenerator.hx
@@ -0,0 +1,63 @@
+// Copyright (c) 2013, 杨博 (Yang Bo)
+// All rights reserved.
+// 
+// Author: 杨博 (Yang Bo) <pop.atry@gmail.com>
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the <ORGANIZATION> nor the names of its contributors
+//   may be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package tests;
+
+using com.dongxiguo.continuation.utils.Generator;
+
+/**
+  @author 杨博
+**/
+@:build(com.dongxiguo.continuation.Continuation.cpsByMeta(":cps"))
+class TestGenerator
+{
+  @:cps
+  static function intGenerator(yield:YieldFunction<Int>):Void
+  {
+    for (i in 1...4)
+    {
+      for (j in 1...(i+1))
+      {
+        trace('$j * $i =');
+        yield(i * j).async();
+        trace("-------");
+      }
+    }
+  }
+
+  public static function main() 
+  {
+    trace(1.5 % 1);
+    for (i in intGenerator)
+    {
+      trace(i);
+    }
+  }
+  
+}
