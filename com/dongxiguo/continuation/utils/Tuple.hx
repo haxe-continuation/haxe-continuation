8d1469fab980be2bd1dcf46e2e3c8342a02dd480
杨博 <pop.atry@gmail.com>
2015-03-15 15:37:52 +0800

    Add Tuple.hx

---
+++ com/dongxiguo/continuation/utils/Tuple.hx
@@ -0,0 +1,48 @@
+// Copyright (c) 2015, 杨博 (Yang Bo)
+// All rights reserved.
+//
+// Author: 杨博 (Yang Bo) <pop.atry@gmail.com>
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the <ORGANIZATION> nor the names of its contributors
+//   may be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package com.dongxiguo.continuation.utils;
+
+/**
+ * @author 杨博
+ */
+class Tuple
+{
+
+  public static inline function pair<A0, A1>(a0:A0, a1:A1, callback:A0->A1->Void):Void
+  {
+    callback(a0, a1);
+  }
+
+  public static inline function triple<A0, A1, A2>(a0:A0, a1:A1, a2:A2, callback:A0->A1->A2->Void):Void
+  {
+    callback(a0, a1, a2);
+  }
+
+}
